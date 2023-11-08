8cda0fbcfdb8fecea41e6178f109fcf1a837ee20
Falconi Franco <100429078+la216677@users.noreply.github.com>
2023-11-08 09:57:08 +0100

    init-project

---
+++ compteurapp/src/main/java/be/helha/compteurapp/CompteurappApplication.java
@@ -0,0 +1,13 @@
+package be.helha.compteurapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompteurappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompteurappApplication.class, args);
+    }
+
+}
---
+++ compteurapp/src/test/java/be/helha/compteurapp/CompteurappApplicationTests.java
@@ -0,0 +1,13 @@
+package be.helha.compteurapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompteurappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
