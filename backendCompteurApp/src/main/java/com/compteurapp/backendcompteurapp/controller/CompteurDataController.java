86f4a12ea25117c6ea4eae6c51c358dbece4e8e7
Zakaria Smaali <101876655+Zakariasma@users.noreply.github.com>
2023-11-19 13:25:15 +0100

    Add methode for get CompteurData when Facture exit or not and if the statement Facture is "IMPAYER"

--- backendCompteurApp/src/main/java/com/compteurapp/backendcompteurapp/controller/CompteurDataController.java
+++ backendCompteurApp/src/main/java/com/compteurapp/backendcompteurapp/controller/CompteurDataController.java
@@ -2,6 +2,7 @@ package com.compteurapp.backendcompteurapp.controller;
 
 import com.compteurapp.backendcompteurapp.exception.ResourceNotFoundException;
 import com.compteurapp.backendcompteurapp.model.CompteurData;
+import com.compteurapp.backendcompteurapp.model.FactureStatement;
 import com.compteurapp.backendcompteurapp.repository.CompteurDataRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,22 @@ public class CompteurDataController {
     @Autowired
     CompteurDataRepository repository;
 
+
+    /**/
+    /* METHODE POUR LE CLIENT"
+    /**/
+
+    @PostMapping("/createCompteurData")
+    public CompteurData createCompteurData(@Valid @RequestBody CompteurData compteurData) {
+        return repository.save(compteurData);
+    }
+
+
+    /**/
+    /* METHODE POUR LE VENDEUR */
+    /**/
+
+    /* On récupere tout les relevés d'un client */
     @GetMapping("/getCompteurDataByClientId/{idClient}")
     public List<CompteurData> getCompteurDataByClientId(@PathVariable Long idClient) {
         List<CompteurData> compteurDataList = repository.findByIdClient(idClient);
@@ -26,19 +43,37 @@ public class CompteurDataController {
         return compteurDataList;
     }
 
-    @GetMapping("/getCompteurDataByVendeurId/{idVendeur}")
-    public List<CompteurData> getCompteurDataByVendeurId(@PathVariable Long idVendeur){
-        List<CompteurData> compteurDataList = repository.findByIdVendeur(idVendeur);
+    /* On récupere les CompteurData pour les relevés pas encore traiter */
+    @GetMapping("/getCompteurDataByVendeurIdWithoutFacture/{idVendeur}")
+    public List<CompteurData> getCompteurDataByVendeurIdWithoutFacture(@PathVariable Long idVendeur){
+        List<CompteurData> compteurDataList = repository.findByIdVendeurAndFacturesIsNull(idVendeur);
         if (compteurDataList.isEmpty()) {
-            throw new ResourceNotFoundException("No CompteurData found with vendeur id " + idVendeur);
+            throw new ResourceNotFoundException("No CompteurData found with vendeur id " + idVendeur + " without Facture");
         }
         return compteurDataList;
     }
 
-    @PostMapping("/createCompteurData")
-    public CompteurData createCompteurData(@Valid @RequestBody CompteurData compteurData) {
-        return repository.save(compteurData);
+    /* On récupere les CompteurData d'un client qui ne sont pas traité */
+    @GetMapping("/getCompteurDataByVendeurIdAndClientIdWithoutFacture/{idVendeur}/{idClient}")
+    public List<CompteurData> getCompteurDataByVendeurIdAndClientIdWithoutFacture(@PathVariable Long idVendeur, @PathVariable Long idClient){
+        List<CompteurData> compteurDataList = repository.findByIdVendeurAndIdClientAndFacturesIsNull(idVendeur, idClient);
+        if (compteurDataList.isEmpty()) {
+            throw new ResourceNotFoundException("No CompteurData found with vendeur id " + idVendeur + " and client id " + idClient + " without Facture");
+        }
+        return compteurDataList;
     }
+
+    /* On récupere les CompteurData ou les factures sont impayé */
+    @GetMapping("/getCompteurDataByVendeurIdAndFactureEtat/{idVendeur}")
+    public List<CompteurData> getCompteurDataByVendeurIdAndFactureEtat(@PathVariable Long idVendeur){
+        List<CompteurData> compteurDataList = repository.findByIdVendeurAndFacturesEtat(idVendeur, FactureStatement.IMPAYER);
+        if (compteurDataList.isEmpty()) {
+            throw new ResourceNotFoundException("No CompteurData found with vendeur id " + idVendeur + " and Facture etat IMPAYER");
+        }
+        return compteurDataList;
+    }
+
+
 
 
 
--- backendCompteurApp/src/main/java/com/compteurapp/backendcompteurapp/repository/CompteurDataRepository.java
+++ backendCompteurApp/src/main/java/com/compteurapp/backendcompteurapp/repository/CompteurDataRepository.java
@@ -1,6 +1,7 @@
 package com.compteurapp.backendcompteurapp.repository;
 
 import com.compteurapp.backendcompteurapp.model.CompteurData;
+import com.compteurapp.backendcompteurapp.model.FactureStatement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,6 +9,9 @@ import java.util.List;
 public interface CompteurDataRepository extends JpaRepository<CompteurData, Long> {
 
     List<CompteurData> findByIdClient(Long idClient);
-    List<CompteurData> findByIdVendeur(Long idVendeur);
+    List<CompteurData> findByFacturesIsNull();
+    List<CompteurData> findByIdVendeurAndFacturesIsNull(Long idVendeur);
+    List<CompteurData> findByIdVendeurAndIdClientAndFacturesIsNull(Long idVendeur, Long idClient);
+    List<CompteurData> findByIdVendeurAndFacturesEtat(Long idVendeur, FactureStatement etat);
 
 }
